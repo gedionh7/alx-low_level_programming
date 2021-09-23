97195d17e3fbf18e3be01b0bea6fa5b7d6744412
Gedion Hailu <56441932+gedionh7@users.noreply.github.com>
2021-09-24 02:12:56 +0300

    ....

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,174 +1,74 @@
-#include <stdlib.h>
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * _calloc - allocate (`size' * `nmemb') bytes and set to 0
- * @nmemb: number of elements
- * @size: number of bytes per element
- *
- * Return: pointer to memory, or NULL if `nmemb' or `size' is 0 or malloc fails
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+void _puts(char *str)
 {
-	unsigned int i;
-	char *p;
-
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb * size; ++i)
-		p[i] = 0;
-	return (p);
+int i = 0;
+while (str[i])
+{
+_putchar(str[i]);
+i++;
 }
-
-/**
- * _strdigit - check if string `s' is composed only of digits
- * @s: string to check
- *
- * Return: 1 if true, 0 if false
- */
-int _strdigit(char *s)
-{
-	if (*s == '-' || *s == '+')
-		++s;
-	while (*s)
-	{
-		if (*s < '0' || *s > '9')
-		{
-			return (0);
-		}
-		++s;
-	}
-	return (1);
 }
-
 /**
- * _puts - print string `s'
- * @s: string to print
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
  */
-void _puts(char *s)
+int _atoi(const char *s)
 {
-	while (*s)
-		_putchar(*(s++));
+int sign = 1;
+unsigned long int resp = 0, firstNum, i;
+for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+{
+if (s[firstNum] == '-')
+{
+sign *= -1;
 }
-
-/**
- * rev_num_str - reverse a number string up to trailing zeros
- * @start: beginning of number
- * @end: end of number
- * @ns: string containing number
- */
-void rev_num_str(int start, int end, char *ns)
-{
-	int i, j;
-	char tmp;
-
-	while (ns[end] == 0 && end != start)
-		--end;
-	for (i = start, j = end; i <= j; ++i, --j)
-	{
-		tmp = ns[i] + '0';
-		ns[i] = ns[j] + '0';
-		ns[j] = tmp;
-	}
 }
-
-/**
- * _strlen - calculate length of string `s'
- * @s: string to get length of
- *
- * Return: length of string
- */
-int _strlen(char *s)
+for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 {
-	int i;
-
-	for (i = 0; s[i]; ++i)
-		;
-	return (i);
+resp *= 10;
+resp += (s[i] - 48);
 }
-
-/**
- * strmul - multply two numbers as strings
- * @a: first number
- * @b: second number
- *
- * Return: pointer to result on success, or NULL on failure
- */
-char *strmul(char *a, char *b)
-{
-	int la, lb, i, j, k, l, neg = 0;
-	char *result;
-	char mul, mul_carry, sum, sum_carry;
-
-	if (*a == '-')
-	{
-		neg ^= 1;
-		++a;
-	}
-	if (*b == '-')
-	{
-		neg ^= 1;
-		++b;
-	}
-	la = _strlen(a);
-	lb = _strlen(b);
-	result = _calloc(la + lb + 1 + neg, sizeof(char));
-	if (result == NULL)
-		return (NULL);
-	if (neg)
-		result[0] = '-';
-	for (i = lb - 1, l = neg; i >= 0; --i, ++l)
-	{
-		mul_carry = 0;
-		sum_carry = 0;
-		for (j = la - 1, k = l; j >= 0; --j, ++k)
-		{
-			mul = (a[j] - '0') * (b[i] - '0') + mul_carry;
-			mul_carry = mul / 10;
-			mul %= 10;
-			sum = result[k] + mul + sum_carry;
-			sum_carry = sum / 10;
-			sum %= 10;
-			result[k] = sum;
-		}
-		result[k] = sum_carry + mul_carry;
-	}
-	rev_num_str(neg, k, result);
-	return (result);
+return (sign *resp);
 }
-
 /**
- * main - multiply two numbers from the command line and print the result
- * @argc: argument count
- * @argv: argument list
- *
- * Return: 0 if successful, 98 if failure
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
  */
-int main(int argc, char *argv[])
+void print_int(unsigned long int n)
 {
-	char *result;
-
-	if (argc != 3)
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	if (!_strdigit(argv[1]) || !_strdigit(argv[2]))
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	result = strmul(argv[1], argv[2]);
-	if (result == NULL)
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	_puts(result);
-	_putchar('\n');
-	free(result);
-	exit(EXIT_SUCCESS);
+unsigned  long int divisor = 1, i, resp;
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+resp = n / divisor;
+_putchar('0' + resp);
+}
+}
+/**
+ * main - print the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+(void)argc;
+if (argc != 3)
+{
+_puts("Error ");
+exit(98);
+}
+print_int(_atoi(argv[1]) * _atoi(argv[2]));
+_putchar('\n');
+return (0);
 }
