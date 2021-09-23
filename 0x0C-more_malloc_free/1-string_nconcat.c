c1890d57ae59aba1fa0acc1e3595e321bd950847
Gedion Hailu <56441932+gedionh7@users.noreply.github.com>
2021-09-23 22:34:26 +0300

    ...

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,30 +1,48 @@
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenate `s1' and the first `n' bytes of `s2'
- * @s1: first string
- * @s2: second string
- * @n: number of bytes from `s2'
- *
- * Return: pointer to concatenated strings, or NULL on failure
- */
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
-	char *p;
+unsigned int i = 0, j = 0, k = 0, l = 0;
+char *str;
 
-	for (i = 0; s1 && s1[i]; ++i)
-		
-	for (j = 0; s2 && s2[j]; ++j)
-		
-	n = (n > j ? j : n);
-	p = malloc(sizeof(char) * (i + n + 1));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; s1 && s1[i]; ++i)
-		p[i] = s1[i];
-	for (j = 0; s2 && j < n; ++j)
-		p[i + j] = s2[j];
-	p[i + j] = '\0';
-	return (p);
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[i])
+i++;
+while (s2[k])
+k++;
+if (n >= k)
+l = i + k;
+else
+l = i + n;
+str = malloc(sizeof(char) * l + 1);
+if (str == NULL)
+return (NULL);
+k = 0;
+while (j < l)
+{
+if (j <= i)
+str[j] = s1[j];
+
+if (j >= i)
+{
+str[j] = s2[k];
+k++;
+}
+j++;
+}
+str[j] = '\0';
+return (str);
 }
